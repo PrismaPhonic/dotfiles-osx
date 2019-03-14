d73dfcc301e4814f8389659a26290679bcdd2dad
PrismaPhonic <Peter@PrismaPhonic.com>
2019-03-14 16:49:59 -0700

    Added three fonts and setup a font install script that gets run from the main program install script

---
+++ install-fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Install all nerd fonts
+cp fonts/Terminus/* ~/Library/Fonts/
+cp fonts/FiraCode/* ~/Library/Fonts/
+cp fonts/Hack/* ~/Library/Fonts/
--- install-programs.sh
+++ install-programs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+## Other Scripts to Run
+## Font Script
+./install-fonts.sh
+
 # Install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 chsh -s /bin/zsh
